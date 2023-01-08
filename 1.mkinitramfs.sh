37c6c5ac4b48e85700f34d71b3b28b937dd6ffb7
Konstantin <oks-mgn@mail.ru>
2023-01-09 02:13:56 +0500

    x86_64 architecure explicit

--- 1.mkinitramfs.sh
+++ 1.mkinitramfs.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Uncomment this if you want create hardlinks instead of copying. It will be faster.
+#if [ $(whoami) == 'root' ]; then
+#    copy_param="-l";
+#fi
+
+
 rm -r initramfs
 mkdir initramfs
 
@@ -10,10 +17,10 @@ ln -s usr/bin initramfs/bin
 
 BINS="dash mount umount mkdir find basename modprobe setfont loadkeys"
 BINS="$BINS ls ln rm cat openvt env bash mount.ntfs mount.ntfs-3g mkfs.ntfs"
-copyso -p $BINS initramfs
+copyso $copy_param --x64 -p $BINS initramfs
 ln -s lib initramfs/lib64
 ln -s x86_64-linux-gnu/ld-linux-x86-64.so.2 initramfs/lib/ld-linux-x86-64.so.2
-cp init initramfs/init
+cp $copy_param init initramfs/init
 chmod +x initramfs/init
 ln -s /usr/bin/dash initramfs/bin/sh
 
@@ -25,7 +32,7 @@ copy_deb()
 		if [ -d $F ]; then
 			mkdir -p initramfs$F
 		elif [ -f $F ]; then
-			copyso $copy_params /$F initramfs
+			copyso $copy_param --x64 /$F initramfs
 		fi
 	done
 }
@@ -60,17 +67,17 @@ MODS="$MODS ehci-pci ohci-pci uhci-hcd xhci-pci usbhid i2c-hid psmouse hid-gener
 MODS="$MODS efivarfs"
 
 copyko $MODS initramfs/lib/modules/$KERVER
-cp -uf /lib/modules/$KERVER/modules.builtin* initramfs/lib/modules/$KERVER
+cp $copy_param /lib/modules/$KERVER/modules.builtin* initramfs/lib/modules/$KERVER
 depmod -b initramfs
 
 # НАСТРОЙКА КОНСОЛИ
 
 mkdir -p initramfs/lib/{terminfo,locale}
-cp -r /lib/terminfo/l initramfs/lib/terminfo
-cp -r /usr/share/mc/{skins,syntax,mc.charsets,mc.lib} initramfs/usr/share/mc
+cp -r $copy_param /lib/terminfo/l initramfs/lib/terminfo
+cp -r $copy_param /usr/share/mc/{skins,syntax,mc.charsets,mc.lib} initramfs/usr/share/mc
 mkdir -p initramfs/usr/lib/locale
-cp /usr/lib/locale/locale-archive initramfs/usr/lib/locale
-cp -r /usr/share/tabset initramfs/usr/share
+cp  $copy_param/usr/lib/locale/locale-archive initramfs/usr/lib/locale
+cp -r $copy_param /usr/share/tabset initramfs/usr/share
 cp $(find /etc/console-setup -name '*.psf.gz' | head -n 1) initramfs/etc/font.psf.gz
 cp $(find /etc/console-setup -name '*.kmap.gz' | head -n 1) initramfs/etc/keys.kmap.gz
 gunzip initramfs/etc/font.psf.gz
